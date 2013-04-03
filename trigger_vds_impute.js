2bcd35faa8daee48c63bfde38da40794d3b0e567
James E. Marca <jmarca@translab.its.uci.edu>
2013-04-03 10:13:38 -0700

    second step

--- trigger_vds_impute.js
+++ trigger_vds_impute.js
@@ -13,69 +13,100 @@ var R;
 
 
 
-var  increment = function(){
-    var years = [2007,2008,2009,2010];
-    var districts = ['/data/pems/breakup/D04'
-                     ,'/data/pems/breakup/D12'
-                     ,'/data/pems/breakup/D07'
-                     ,'/data/pems/breakup/D05'
-                     ,'/data/pems/breakup/D06'
+var  increment = function(keys){
+    var years = [2007,2008,2009,2010,2011];
+    var districts = [//'/data/pems/breakup/D04'
+                     //,'/data/pems/breakup/D07'
+                     //,'/data/pems/breakup/D12'
+                     //,'/data/pems/breakup/D05'
+                     //,'/data/pems/breakup/D06'
                      ,'/data/pems/breakup/D08'
-                     ,'/data/pems/breakup/D03'
-                     ,'/data/pems/breakup/D11'
-                     ,'/data/pems/breakup/D10']
-    var yidx = -1;
-    var didx = 100;
+                     //,'/data/pems/breakup/D03'
+                     //,'/data/pems/breakup/D11'
+                     //,'/data/pems/breakup/D10'
+    ]
+    var yidx = []
+    var didx = []
+    _.each(keys
+           ,function(k){
+               yidx[k]=-1;
+               didx[k]=100;
+           })
 
-    return function(){
-        if(didx >= districts.length){
+    function _increment(k){
+        if(didx[k] >= districts.length - 1){
             // increment year, reset didx
-            yidx += 1;
-            didx=0;
+            yidx[k] += 1;
+            didx[k] = 0;
         }else{
-            didx += 1;
+            didx[k] += 1;
         }
-        if(yidx >= years.length){
+        if(yidx[k] >= years.length){
             return false;
         }else{
-            process.env['RYEAR'] = years[yidx];
-            process.env['RDISTRICT'] =  districts[didx]
             return true;
         }
     }
-}();
+    _increment.getDistrict=function(k){
+        if(yidx[k] >= years.length)
+            return null;
+        return districts[didx[k]];
+    }
+    _increment.getYear=function(k){
+        if(yidx[k] >= years.length)
+            return null;
+        return years[yidx[k]];
+    }
+    return _increment;
+}([1,2]);
 
+
+var RCall = {1:['--no-restore','--no-save','vds_self_impute_missing_distributed.R']
+             //,2:['--no-restore','--no-save','vds_imputation_prepwork.R']
+             ,2:['--no-restore','--no-save','vds_self_impute_missing_distributed.R']
+            };
 
 function loop(f){
-    var logstream = fs.createWriteStream('vdsimpute_log_'+f+'.log', { flags: 'a',
-                                                                      encoding: 'utf8',
-                                                                      mode: 0666 });
 
     function _loop(cb){
 
-        var opts = { cwd: undefined,
-                     env: process.env
-                   }
-        opts.env['RREVERSE']=f;
-
-        R  = spawn('Rscript', ['--no-restore','--no-save','vds_self_impute_missing_distributed.b.R'],opts);
-        R.stderr.setEncoding('utf8');
-        R.stdout.setEncoding('utf8');
-
-        R.stdout.pipe(logstream)
-        R.stderr.pipe(logstream)
-
-        R.on('exit', function (code) {
-            if (code == 10) {
-                console.log('got exit: done, repeating');
-                async.nextTick(_loop(cb))
+        function exithandler(code) {
+            if (code == 10 || code == 1) {
+                console.log('got exit: '+code+', repeating');
+                async.nextTick(innerloop)
             }else{
                 console.log('got exit, code is '+code);
                 cb()
             }
             return null;
-        });
+        }
 
+
+        function innerloop(){
+            console.log('looping '+f);
+            var opts = { cwd: undefined,
+                         env: process.env
+                       }
+            opts.env['RREVERSE']=f;
+            opts.env['RYEAR'] = increment.getYear(f);
+            opts.env['RDISTRICT'] =  increment.getDistrict(f);
+
+            R  = spawn('Rscript', RCall[f],opts);
+            R.stderr.setEncoding('utf8');
+            R.stdout.setEncoding('utf8');
+
+            var logstream = fs.createWriteStream('vdsimpute_log_'+f+'.log', { flags: 'a',
+                                                                              encoding: 'utf8',
+                                                                              mode: 0666 });
+
+
+            R.stdout.pipe(logstream)
+            R.stderr.pipe(logstream)
+
+            R.on('exit',exithandler);
+
+        }
+        async.nextTick(innerloop)
     }
     return _loop;
 };
@@ -93,11 +124,16 @@ var dual_R_calls = [loop(1)
                     }];
 
 
-async.whilst(increment,
-       function(whcb){
-           async.parallel(dual_R_calls,whcb);
-       }
-       ,function(err){
-           console.log('alldone');
-       } );
+async.whilst(function(){return increment(1);}
+             ,loop(1)
+             ,function(err){
+                 console.log('alldone with 1');
+             } );
+async.whilst(function(){return increment(2);}
+             ,loop(2)
+             ,function(err){
+                 console.log('alldone with 2');
+             } );
 
+
+1;
