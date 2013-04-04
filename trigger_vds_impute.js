1f1ea5a82491422888cf2e1ef298724640120832
James E. Marca <jmarca@translab.its.uci.edu>
2013-04-03 23:32:37 -0700

    add year to log file name in vds impute trigger output too

--- trigger_vds_impute.js
+++ trigger_vds_impute.js
@@ -58,7 +58,7 @@ var trigger_R_job = function(task,done){
     var R  = spawn('Rscript', RCall, opts);
     R.stderr.setEncoding('utf8')
     R.stdout.setEncoding('utf8')
-    var logfile = 'log/vdsimpute_log_'+did+'.log'
+    var logfile = 'log/vdsimpute_'+did+'_'+opts.env['RYEAR']+'.log'
     var logstream = fs.createWriteStream(logfile
                                         ,{flags: 'a'
                                          ,encoding: 'utf8'
