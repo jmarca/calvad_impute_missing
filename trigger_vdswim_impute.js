e1d8ce8117685f6995f2b4075e37ff5add3a71b5
James E. Marca <jmarca@translab.its.uci.edu>
2013-04-05 16:19:34 -0700

    ready to start testing vdswim_impute
    sorted through the code, and it looks okay, time to test
    
    added trigger_vdswim_impute.js
    added vdswim_impute.R
    
    mostly refactoring from bdp code

---
+++ trigger_vdswim_impute.js
@@ -0,0 +1,231 @@
+/**
+ * trigger vdswim impute, which means guessing truck traffic for vds sites
+ *
+ * the logic is similar to others, but a bit more involved.  instead
+ * of using R to check for files, use node, and then send in filenames
+ * to R via environment variables, as before.
+ *
+ * I need to get filenames for
+ *
+ * vds year imputed RData
+ * for all neighbor wim/vds sites
+ *    vds wim paired RData
+ *
+ * hmm, that last is a problem to pass via env vars.
+ *
+ * perhaps just make sure that the things exist, and let R do the work.
+ *
+ *
+ * Need to make sure
+ *   ## check if we're already done with this one
+ *   if(couch.check.is.truck.imputed(district,year,vdsid)){
+ *      print('truck imputed is done or inprocess')
+ *      return()
+ *   }
+ *
+ *   if(!couch.checkout.for.processing.truck.imputed(district,year,vdsid)){
+ *      print('failed to checkout for processing')
+ *      return()
+ *   }
+ *
+ *   if(! couch.check.is.raw.imputed(year,vdsid) ){
+ *      print('raw data is not imputed')
+ *      ## if the raw data isn't ready, can't do anything here
+ *      return()
+ *   }
+ *
+ */
+
+
+var util  = require('util'),
+    spawn = require('child_process').spawn;
+var path = require('path');
+var fs = require('fs');
+var async = require('async');
+var _ = require('lodash');
+var get_files = require('./get_files')
+var suss_detector_id = require('suss_detector_id')
+var couch_check = require('couch_check_state')
+var couch_set   = require('couch_set_state')
+
+var statedb = 'vdsdata%2ftracking'
+
+var R;
+
+
+var finish_regex = /finish/;
+var date=new Date()
+var inprocess_string = process.env.INPROCESS_STRING || date.toISOString()+' inprocess'
+var finish_string = process.env.FINISH_STRING || date.toISOString()+' finish'
+
+/**
+ * refactor items
+ *
+ * 1. get files here
+ * 2. send just a single file to R for processing
+ * 3. which means make sure that each file that is sent to R really
+ *    needs processing
+ *
+ */
+
+function vdsfile_handler(opt){
+
+    return function(f,cb){
+        var did = suss_detector_id(f)
+        // need to check that truck vols have not yet been imputed
+        couch_check({'db':statedb
+                    ,'doc':did
+                    ,'year':opt.env['RYEAR']
+                    ,'state':'truckimputed'
+                    }
+                   ,function(err,state){
+                        if(err) return cb(err)
+                        if(state && (finish_regex.test(state)) || state === inprocess_string){
+                            return cb()
+                        }
+                        // perhaps in process for the current process
+                        // need to verify that the raw imputation is okay
+                        couch_check({'db':statedb
+                                    ,'doc':did
+                                    ,'year':opt.env['RYEAR']
+                                    ,'state':'vdsraw_chain_lengths'
+                                    }
+                                   ,function(err,state){
+                                        if(err) return cb(err)
+                                        if(state && _.isArray(state) && state.length==5){
+                                            // the raw data is okay to proceed
+                                            file_queue.push({'file':f
+                                                            ,'opts':opt})
+                                        }
+                                        return cb(err)
+                                    })
+                        return null
+                    })
+        return null
+    }
+}
+
+
+var setup_R_job = function(task,done){
+    var file = task.file
+    var did = suss_detector_id(file)
+    var opts = _.clone(task.opts,true)
+    // need to check again that truck vols have not yet been imputed
+    couch_check({'db':statedb
+                ,'doc':did
+                ,'year':opts.env['RYEAR']
+                ,'state':'truckimputed'
+                }
+               ,function(err,state){
+                    if(err) return done(err)
+                    if(state && (finish_regex.test(state)) || state === inprocess_string){
+                        return done()
+                    }
+                    // check out for processing
+                    couch_set({'db':statedb
+                              ,'doc':did
+                              ,'year':opts.env['RYEAR']
+                              ,'state':'truckimputed'
+                              ,'value':inprocess_string
+                              }
+                             ,function(err){
+                                  if(err) return done(err)
+                                  return trigger_R_job(task,done)
+                              })
+                    return null
+                })
+    return null
+}
+
+
+var trigger_R_job = function(task,done){
+    var file = task.file
+    console.log('processing '+file)
+    var did = suss_detector_id(file)
+    var opts = _.clone(task.opts)
+    opts.env['FILE']=file
+
+    var R  = spawn('Rscript', RCall, opts);
+    R.stderr.setEncoding('utf8')
+    R.stdout.setEncoding('utf8')
+    var logfile = 'log/vdswim_impute_'+did+'_'+opts.env['RYEAR']+'.log'
+    var logstream = fs.createWriteStream(logfile
+                                        ,{flags: 'a'
+                                         ,encoding: 'utf8'
+                                         ,mode: 0666 })
+    R.stdout.pipe(logstream)
+    R.stderr.pipe(logstream)
+    R.on('exit',function(code){
+        console.log('got exit: '+code+', for ',did)
+        if(code==10){
+                    couch_set({'db':statedb
+                              ,'doc':did
+                              ,'year':opts.env['RYEAR']
+                              ,'state':'truckimputed'
+                              ,'value':finish_string
+                              }
+                             ,function(err){
+                                  if(err) throw new Error(err)
+                                  throw new Error('die in testing')
+                                  return done()
+                              })
+        }else{
+            throw new Error('die in testing')
+            return done()
+        }
+    })
+}
+
+var file_queue=async.queue(trigger_R_job,2)
+
+var years = [2007,2008,2009,2010,2011];
+
+var districts = ['D04'
+                ,'D08'
+                ,'D12'
+                ,'D05'
+                ,'D06'
+                ,'D07'
+                ,'D03'
+                ,'D11'
+                ,'D10'
+                ]
+
+
+var RCall = ['--no-restore','--no-save','vdswim_impute.R']
+
+
+var opts = { cwd: undefined,
+             env: process.env
+           }
+var years_districts = []
+_.each(years,function(year){
+    _.each(districts,function(district){
+        var o = _.clone(opts,true)
+        o.env['RYEAR'] = year
+        o.env['RDISTRICT']=district
+        years_districts.push(o)
+    })
+});
+
+// debugging, just do one combo for now
+// years_districts=[years_districts[0]]
+async.eachLimit(years_districts,2,function(opt,cb){
+    // get the files
+    var handler = vdsfile_handler(opt)
+    get_files.get_yearly_vdsfiles({district:opt.env['RDISTRICT']
+                                  ,year:opt.env['RYEAR']
+                                  ,'rdata':1}
+                                 ,function(err,list){
+                                      if(err) throw new Error(err)
+                                      async.each(list
+                                                ,handler
+                                                ,cb);
+                                      return null
+                                  });
+});
+
+
+
+
+1;
---
+++ vdswim_impute.R
@@ -0,0 +1,258 @@
+server <- "http://calvad.ctmlabs.net"
+vds.service <- 'vdsdata'
+wim.service <- 'wimdata'
+#vds.path <- "/data/pems/breakup"
+#wim.path <- "/data/wim"
+output.path <- "./imputed"
+fschecks <- FALSE
+source('components/jmarca-rstats_couch_utils/couchUtils.R')
+source('components/jmarca-rstats_remote_files/remoteFiles.R')
+source('components/jmarca-calvad_rscripts/lib/get.medianed.amelia.vds.R')
+source('components/jmarca-calvad_rscripts/lib/amelia_plots_and_diagnostics.R')
+
+source('components/jmarca-calvad_rscripts/lib/process.raw.pems.file.R')
+source('components/jmarca-calvad_rscripts/lib/wim.loading.functions.R')
+source('components/jmarca-calvad_rscripts/lib/wim.pre.processing.R')
+source("components/jmarca-calvad_rscripts/lib/vds.processing.functions.R")
+source('components/jmarca-calvad_rscripts/lib/wim.vds.processing.functions.R')
+source('components/jmarca-calvad_rscripts/lib/just.amelia.call.R')
+source('components/jmarca-calvad_rscripts/lib/pool.wim.vds.R')
+
+
+library('RPostgreSQL')
+m <- dbDriver("PostgreSQL")
+## requires environment variables be set externally
+psqlenv = Sys.getenv(c("PSQL_HOST", "PSQL_USER", "PSQL_PASS"))
+
+con <-  dbConnect(m
+                  ,user=psqlenv[2]
+                  ,password=psqlenv[3]
+                  ,host=psqlenv[1]
+                  ,dbname="spatialvds")
+
+
+
+## pass in the vdsid and the year
+
+impute.vds.site <- function(vdsid,year,vdsfile){
+
+  print(paste('processing ',paste(entry,collapse=', ')))
+  ## I need to check this.  I thought entry contained both the id and the direction
+  district <- district.from.vdsid(vdsid)
+
+  ## load the vds data
+  df.vds.zoo <- get.zooed.vds.amelia(vdsid,serverfile=vdsfile)
+  if(is.null(df.vds.zoo)){
+    exit(0)
+  }
+
+  ## standard unzoo incantation
+  df.vds <- unzoo.incantation(df.vds.zoo)
+  rm(df.vds.zoo)
+  ## so that I can pluck out just this site's data at the end of imputation
+  df.vds[,'vds_id'] <- vdsid
+  ## probably not necessary yet
+  ## gc()
+
+  ## pick off the lane names so as to drop irrelevant lanes in the loop below
+  vds.names <- names(df.vds)
+  vds.nvars <- grep( pattern="^n(l|r)\\d+",x=vds.names,perl=TRUE,value=TRUE)
+
+
+  #####################
+  ## loading WIM data paired with VDS data from WIM neighbor sites
+  ######################
+  bigdata <- data.frame()
+  wim.ids <- get.list.neighbor.wim.sites(vdsid)
+  for( wii in 1:length(wim.ids$wim_id) ){
+    wim.id <- wim.ids[wii,'wim_id']
+    wim.dir <- wim.ids[wii,'direction']
+    paired.vdsids <- wim.vds.pairs[wim.vds.pairs$wim_id==wim.id  & wim.vds.pairs$direction==wim.dir,'vds_id']
+    for(paired.vdsid in paired.vdsids){
+      paired.RData <- get.RData.view(paired.vdsid,year)
+      if(length(paired.RData)==0) { next }
+      result <- couch.get.attachment(trackingdb,paired.vdsid,paired.RData)
+      load(result)
+      df.merged <- tempfix.borkborkbork(df.merged)
+      ## convention over configuration I guess.  These files are always called df.merged
+      ic.names <- names(df.merged)
+      keep.columns = intersect(c( "ts","tod","day","imp","vds_id" ),ic.names)
+      ## use vds.nvars to drop unwanted lanes
+      for( lane in 1:length(vds.nvars) ){
+        pattern = paste(substring(vds.nvars[lane],2)[1],'$',sep='')
+        keep.columns.lane <-  grep( pattern=pattern,x=ic.names,perl=TRUE,value=TRUE)
+        if(length(keep.columns) == 0){
+          keep.columns = keep.columns.lane
+        }else{
+          keep.columns = union(keep.columns,keep.columns.lane)
+        }
+      }
+      df.merged <- df.merged[,keep.columns]
+      ## merge it
+      if(length(bigdata)==0){
+        bigdata <-  df.merged
+      }else{
+        ## here I need to make sure all WIM-VDS sites have similar lanes
+        ## the concern is a site with *fewer* lanes than the vds site
+        ic.names <- names(df.merged)
+        bigdata.names <- names(bigdata)
+        ## keep the larger of the two
+        common.names <- intersect(ic.names,bigdata.names)
+        bigdata <- bigdata[,common.names]
+        df.merged <- df.merged[,common.names]
+        bigdata <- rbind( bigdata, df.merged )
+      }
+      rm(df.merged)
+
+    }
+  }
+
+  print('concatenating merged and to-do data sets')
+  if(dim(bigdata)[1] < 100){
+    print('bigdata looking pretty empty')
+    stop()
+  }
+  ## bigdata <- concatenate.two.sites(bigdata,aout.agg,maximp=keepimp)
+  wimsites.names <-  names(bigdata)
+  vds.names <- names(df.vds)
+  miss.names.wim <- setdiff(wimsites.names,vds.names)
+  miss.names.vds <- setdiff(vds.names,wimsites.names)
+  ## could be more lanes at the VDS site, for example
+  if(length(miss.names.vds)>0){
+    bigdata[,miss.names.vds] <- NA
+  }
+  ## of course this will be necessary, as the wimsites have truck data and the vds does not
+  df.vds[,miss.names.wim] <- NA
+
+  ## merge into bigdata
+  bigdata <- rbind(bigdata,df.vds)
+  miss.names.vds <- union(miss.names.vds,c('vds_id'))
+  i.hate.r <- c(miss.names.vds,'nr1') ## need a dummy index or R will simplify
+  holding.pattern <- bigdata[,i.hate.r]
+
+  this.vds <- bigdata['vds_id'] == vdsid
+  this.vds <- !is.na(this.vds)  ## lordy I hate when NA isn't falsey
+
+  for(i in miss.names.vds){
+    bigdata[,i] <- NULL
+  }
+  rm(df.vds)
+  gc()
+
+  ## bugfix.  vds amelia runs might have been done with improper
+  ## limits on occ.  Very old runs only, but need to fix here
+  occ.pattern <- "^o(l|r)\\d$"
+  occ.vars <-  grep( pattern=occ.pattern,x=names(bigdata),perl=TRUE,value=TRUE)
+  ## truncate mask
+  toobig <-  !( bigdata[,occ.vars]<1 | is.na(bigdata[,occ.vars]) )
+  bigdata[,occ.vars][toobig] <- 1
+
+  ## run amelia to impute missing (trucks)
+  print('all set to impute')
+  big.amelia <- fill.truck.gaps(bigdata)
+
+
+  ## I used to save here, but now that this is distributed, I should
+  ## probably convert to csv and push to psql sooner
+
+  ## write out the imputation chains information to couchdb for later analysis
+  ## and also generate plots as attachments
+
+  itercount <- store.amelia.chains(big.amelia,year,vdsid,'truckimputation')
+
+
+  ## extract just this vds_id data and
+  ## put back any variables I took out above
+
+  df.amelia.c <- big.amelia$imputations[[1]][this.vds,]
+  df.amelia.c[,miss.names.vds] <- holding.pattern[this.vds,miss.names.vds]
+
+  ## limit to what I did impute only
+  varnames <- names(df.amelia.c)
+  var.list <- names.munging(varnames)
+  keep.names <- setdiff(varnames,var.list$exclude.as.id.vars)
+  keep.names <- union(keep.names,c('ts','tod','day','vds_id'))
+  df.amelia.c <- df.amelia.c[,keep.names]
+
+  if(length(big.amelia$imputations) > 1){
+    for(i in 2:length(big.amelia$imputations)){
+      temp <- big.amelia$imputations[[i]][this.vds,]
+      temp[,miss.names.vds] <- holding.pattern[this.vds,miss.names.vds]
+      temp <- temp[,keep.names]
+      df.amelia.c <- rbind(df.amelia.c,temp)
+    }
+  }
+  ## get rid of stray dots in variable names
+  db.legal.names  <- gsub("\\.", "_", names(df.amelia.c))
+  names(df.amelia.c) <- db.legal.names
+
+  ## unsure about this.  seems like lots of NA values could likely be produced.
+  df.amelia.c.l <- transpose.lanes.to.rows(df.amelia.c)
+
+  ## okay, actually write the csv file
+  filename <- paste('vds_id',vdsid,'truck.imputed',year,'csv',sep='.')
+  ## don't prior imputations
+  exists <- dir(output.path,filename)
+  tick <- 0
+  while(length(exists)==1){
+    tick = tick+1
+    filename <- paste('vds_id',vdsid,'truck.imputed',year,tick,'csv',sep='.')
+    ## don't overwrite files
+    exists <- dir(output.path,filename)
+  }
+  file <- paste(output.path,filename,sep='/')
+
+  write.csv(df.amelia.c.l,file=file,row.names = FALSE)
+  ## run perl code to slurp output
+  system2('perl',paste(' -w /home/james/repos/bdp/parse_imputed_vds_trucks_to_couchDB.pl --cdb=imputed/breakup/ --file=',file,sep='')
+          ,stdout = FALSE, stderr = paste(output.path,paste(vdsid,year,'parse_output.txt',sep='.'),sep='/'),wait=FALSE)
+
+  ## while that runs, make some plots
+
+  ## generate a df for plots.  Use median here, because that is what I will do with final output
+  df.amelia.zoo <- medianed.aggregate.df(df.amelia.c,median)
+  df.med <- unzoo.incantation(df.amelia.zoo)
+  rm(df.amelia.zoo)
+  make.truck.plots(df.med,year,vdsid,'vds',vdsid,imputed=TRUE)
+  rm(df.med)
+
+  ## again, a save on a remote stystem is useles.  move on to csv,
+  ## possibly push to psql or couchdb
+
+  make.truck.plots.by.lane(df.amelia.c.l,year,vdsid,'vds',vdsid,imputed=TRUE)
+  quit(save='no',status=10)
+  
+}
+
+
+
+district = Sys.getenv(c('RDISTRICT'))[1]
+if(is.null(district)){
+  print('assign a district to the RDISTRICT environment variable')
+  exit(1)
+}
+
+vdsfile = Sys.getenv(c('FILE'))[1]
+if(is.null(vdsfile)){
+  print('assign a file to process to the FILE environment variable')
+  exit(1)
+}
+
+year = as.numeric(Sys.getenv(c('RYEAR'))[1])
+if(is.null(year)){
+  print('assign the year to process to the RYEAR environment variable')
+  exit(1)
+}
+
+seconds <- 3600
+
+wim.vds.pairs <- get.list.closest.wim.pairs()
+
+file.names <- strsplit(file,split="/")
+file.names <- file.names[[1]]
+fname <-  strsplit(file.names[length(file.names)],"\\.")[[1]][1]
+vds.id <-  get.vdsid.from.filename(fname)
+
+
+impute.vds.site(vds.id,year,vdsfile=vdsfile)
+
