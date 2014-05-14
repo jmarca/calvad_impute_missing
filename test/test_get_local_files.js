384249ab64c20ce2140e53cbf9a1342e2ba58607
James Marca <jmarca@translab.its.uci.edu>
2014-05-13 21:07:57 -0700

    split local and remote tests
    new file:   test/test_get_local_files.js
    renamed:    test/test_get_files.js -> test/test_get_remote_files.js

---
+++ test/test_get_local_files.js
@@ -0,0 +1,65 @@
+// test the db path
+
+/* global require console process describe it */
+
+var should = require('should')
+
+var async = require('async')
+var _ = require('lodash')
+var superagent = require('superagent')
+var http = require('http')
+
+var get_files = require('../get_files')
+
+
+describe('get vds files local',function(){
+    it('should get txt files in 2010, D05'
+      ,function(done){
+           get_files.get_yearly_vdsfiles_local({'district':'D05'
+                                               ,'year':2010}
+                                              ,function(err,list){
+                                                   should.not.exist(err)
+                                                   //console.log(list)
+                                                   list.should.have.property('length',10)
+                                                   _.each(list
+                                                         ,function(f){
+                                                              f.should.match(/\.txt\.?.z$/);
+                                                          })
+                                                       return done()
+                                               })
+       })
+    it('should get rdata files in 2007, D05'
+      ,function(done){
+           var yr = 2010
+           get_files.get_yearly_vdsfiles_local({'district':'D05'
+                                               ,'year':yr
+                                               ,'rdata':1}
+                                              ,function(err,list){
+                                                   should.not.exist(err)
+                                                   //console.log(list)
+                                                   list.should.have.property('length',10)
+                                                   _.each(list
+                                                         ,function(f){
+                                                              f.should.match(new RegExp(yr+'RData$'));
+                                                          })
+                                                       return done()
+                                               })
+       })
+    it('should get amelia files in 2010, D03'
+      ,function(done){
+           var yr = 2010
+           get_files.get_yearly_vdsfiles_local({'district':'D03'
+                                               ,'year':yr
+                                               ,'amelia':1}
+                                              ,function(err,list){
+                                                   should.not.exist(err)
+                                                   //console.log(list)
+                                                   list.should.have.property('length',10)
+                                                   _.each(list
+                                                         ,function(f){
+                                                              f.should.match(new RegExp(yr+'RData$'));
+                                                          })
+                                                       return done()
+                                               })
+       })
+})
--- test/test_get_remote_files.js
+++ test/test_get_remote_files.js
@@ -45,55 +45,21 @@ describe('get vds files',function(){
                                              return done()
                                          })
        })
-    it('should get rdata files in 2007, D05'
+    it('should get amelia files in 2007, D05'
       ,function(done){
            var yr = 2007
-           get_files.get_yearly_vdsfiles({'district':'D05'
+           get_files.get_yearly_vdsfiles({'district':'D03'
                                          ,'year':yr
                                          ,'amelia':1}
                                         ,function(err,list){
                                              should.not.exist(err)
                                              //console.log(list)
-                                             list.should.have.property('length',10)
+                                             list.should.have.property('length',314)
                                              _.each(list
                                                    ,function(f){
                                                         f.should.match(new RegExp(yr+'RData$'));
                                                     })
                                              return done()
                                          })
-       })
-})
-describe('get vds files local',function(){
-    it('should get txt files in 2010, D05'
-      ,function(done){
-           get_files.get_yearly_vdsfiles_local({'district':'D05'
-                                               ,'year':2010}
-                                              ,function(err,list){
-                                                   should.not.exist(err)
-                                                   //console.log(list)
-                                                   list.should.have.property('length',10)
-                                                   _.each(list
-                                                         ,function(f){
-                                                              f.should.match(/\.txt\.?.z$/);
-                                                          })
-                                                       return done()
-                                               })
-       })
-    it('should get rdata files in 2007, D05'
-      ,function(done){
-           var yr = 2010
-           get_files.get_yearly_vdsfiles_local({'district':'D05'
-                                               ,'year':yr
-                                               ,'rdata':1}
-                                              ,function(err,list){
-                                                   should.not.exist(err)
-                                                   //console.log(list)
-                                                   list.should.have.property('length',10)
-                                                   _.each(list
-                                                         ,function(f){
-                                                              f.should.match(new RegExp(yr+'RData$'));
-                                                          })
-                                                       return done()
-                                               })
        })
 })
