19bee60a00ac25bfe9a086c32c07ba3cae97a795
James E. Marca <james@activimetrics.com>
2015-03-30 08:31:14 -0700

    move raw plot into calvad_rstats package

--- vds_plots.R
+++ vds_plots.R
@@ -8,63 +8,9 @@ library('RCurl')
 library('RJSONIO')
 
 source('node_modules/rstats_couch_utils/couchUtils.R',chdir=TRUE)
-
-source('node_modules/calvad_rscripts/lib/vds_impute.R',chdir=TRUE)
-
 source('node_modules/calvad_rscripts/lib/get.medianed.amelia.vds.R',chdir=TRUE)
 
 
-plot.raw.data <- function(fname,f,path,year,vds.id,remote=FALSE,force.plot=FALSE){
-  ## plot the data out of the detector
-  fileprefix='raw'
-  subhead='raw data'
-  if(!force.plot){
-      have.plot <- check.for.plot.attachment(vds.id,year,fileprefix,subhead)
-      if(have.plot){
-          print('already have plots')
-          return (1)
-      }
-  }
-
-  ## fname is the filename for the vds data.
-  ## f is the full path to the file I need to grab
-
-  ## is there a df available?
-  ts <- data.frame()
-  df <- data.frame()
-  ## df.pattern =paste('**/',fname,'*df*',year,'RData',sep='')
-  ##rdata.file <- make.amelia.output.file(path,fname,seconds,year)
-  if(remote){
-      fetched <- fetch.remote.file(server,service='vdsdata',root=path,file=f)
-      r <- try(result <- load(file=fetched))
-      if(class(r) == "try-error") {
-          print (paste('need to get the raw file.  hold off for now'))
-          return (FALSE)
-      }
-      unlink(x=fetched)
-  }else{
-      df <- load.file(f,fname,year,path)
-  }
-  ## break out ts
-  ts <- df$ts
-  df$ts <- NULL
-  ## aggregate up to an hour?
-  df.vds.agg <- vds.aggregate(df,ts,seconds=3600)
-  if(is.null(dim(df.vds.agg))) return (FALSE)
-
-  files.to.couch <- plot.vds.data(df.vds.agg,
-                                  vds.id,year,
-                                  fileprefix,subhead,
-                                  force.plot=force.plot)
-
-  for(f2a in files.to.attach){
-      couch.attach('vdsdata%2ftracking',vds.id,f2a)
-  }
-
-  rm(df)
-  gc()
-  return (TRUE)
-}
 
 district = Sys.getenv(c('RDISTRICT'))[1]
 if(is.null(district)){
@@ -87,7 +33,11 @@ if(is.null(year)){
 server <- "http://localhost/calvad"
 vds.service <- 'vdsdata'
 
-
+trackingdb <- Sys.getenv(c('COUCHDB_TRACKINGDB'))[1]
+if(is.null(trackingdb)){
+  print('default setting tracking db to vdsdata/tracking')
+  trackingdb <-  'vdsdata%2ftracking'
+}
 
 district.path=paste(district,'/',sep='')
 
@@ -108,8 +58,19 @@ if(is.null(force.plot) || force.plot==0 || force.plot == 'false'){
     force.plot=TRUE
 }
 print(paste('force plot = ',force.plot))
-result <- plot.raw.data(fname,thefile,path,year,vds.id)#,force.plot=force.plot)
+result <- plot.raw.data(fname,thefile,path,year,vds.id
+                       ,remote=FALSE
+                       ,force.plot=force.plot
+                       ,trackingdb=trackingdb)
 
-result <- get.and.plot.vds.amelia(vds.id,year=year,path=path,remote=FALSE,force.plot=force.plot)
+print(paste('done with raw plots, result is',result))
+
+result <- get.and.plot.vds.amelia(vds.id,year=year,doplots=TRUE,
+                                  remote=FALSE,
+                                  path=path,
+                                  force.plot=force.plot,
+                                  trackingdb=trackingdb)
+
+print(paste('done with raw plots, result is',result))
 
 quit(save='no',status=10)
