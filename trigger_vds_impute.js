b14d1ac1d1ae8e1d674298dc83ad63d90ad0f5e6
James E. Marca <james@activimetrics.com>
2016-06-15 15:03:14 -0700

    add mimimum file entry as with truck imputation
    can't use vdsid, but instead use file name (complete path).
    
    This will break if multiple districts are being run, so don't do that.

--- trigger_vds_impute.js
+++ trigger_vds_impute.js
@@ -45,6 +45,37 @@ console.log('setting configuration file to ',config_file,'.  Change with the --c
 
 if(argv.rdata){ opts.rdata=true }
 
+/**
+ * parse configuration parameters
+ *
+ * The config.json parameters should contain postgresql and couchdb
+ * connection information.  In addition, this program is going to look
+ * for
+ *
+ * {calvad:{districts:  array or single string, in the form 'D03'
+ *         ,years: array or a single number,
+ *         ,double_check_amelia: true means to re-examine amelia output, and redo
+ *                               those with a "fake" or "error" amelia result
+ *         ,start_vdsid: whether double checking or not, the first
+ *               **file path** to start on, which typically is a freeway
+ *               number.  Use zero to do them all; use 50 to skip, say, all
+ *               freeways that begin with 49 or lower, etc.  Put the
+ *               complete filename of the last known file to have a good
+ *               processing run.  Note that unlike the vds truck impute
+ *               code, this is the file name (full path), as is written in
+ *               the log file after the "test" statement, so for example,
+ *               test 50/E/FOLSOM_BLVD/318550_HV_2012.txt.xz you would write
+ *               start_vdsid:"50/E/FOLSOM_BLVD/318550_HV_2012.txt.xz" and
+ *               every file that sorted less than that will get skipped and
+ *               every file that sorted greater than or equal to that will
+ *               get processed.
+ *  }}
+ *
+ * @param {} cb
+ *
+ * @returns {null} nothing at all, but will call the callback with
+ * null,config object if success in reading the config file
+ */
 function _configure(cb){
     if(config === undefined){
         config_okay(config_file,function(e,c){
